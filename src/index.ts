43da40f60a5a6a8ed8fcc0190d4a3f5d4618b7e1
James Manners <james@manners.net.au>
2019-04-12 14:37:50 +1000

    Converted to typescript and  added autodetection of instances

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,31 @@
-'use strict';
-
 // Load AWS APIs
-const AWS = require('aws-sdk');
-// TODO enable different regions.
-AWS.config.update({region: 'us-east-1'});
-const lightsail = new AWS.Lightsail();
+import * as AWS from "aws-sdk";
+import { ScheduledHandler } from "aws-lambda";
 
-/**
- * (Required) List of target instances that should be backed up. Provide via
- * environment variable. Should be a comma-separated list of instance names
- * (no spaces).
- * @type {String}
- */
-const BACKUP_INSTANCES = process.env.BACKUP_INSTANCES.split(',') || [];
+const region = process.env.AWS_REGION || "ap-southeast-2";
+AWS.config.update({ region });
+const lightsail = new AWS.Lightsail();
 
 /**
  * How many days to keep daily backups. Can be overridden via
  * environment variable.
  * @type {Number}
  */
-const BACKUP_DAYS = process.env.BACKUP_DAYS || 14;
+const BACKUP_DAYS = +process.env.BACKUP_DAYS || 14;
 
 /**
  * How many weeks to keep weekly backups. Can be overridden via
  * environment variable.
  * @type {Number}
  */
-const BACKUP_WEEKS = process.env.BACKUP_WEEKS || 12;
+const BACKUP_WEEKS = +process.env.BACKUP_WEEKS || 12;
 
 /**
  * How many months to keep monthly backups. Can be overridden via
  * environment variable.
  * @type {Number}
  */
-const BACKUP_MONTHS = process.env.BACKUP_MONTHS || 12;
+const BACKUP_MONTHS = +process.env.BACKUP_MONTHS || 12;
 
 /**
  * Convenience for calculating dates
@@ -47,38 +39,42 @@ const ONE_DAY = 1000 * 60 * 60 * 24;
  * Store backups chronologically, keyed by instance name.
  * @type {Object}
  */
-let backups = {};
-
+let backups: { [key: string]: AWS.Lightsail.InstanceSnapshot[] } = {};
 
 /**
  * Main handler from AWS lambda.
  */
-exports.handler = (event, context, callback) => {
-  (async function(){
-    await loadBackups();
+exports.handler = (async () => {
+  const instances = process.env.BACKUP_INSTANCES
+    ? process.env.BACKUP_INSTANCES.split(",")
+    : await getAllInstances();
+  await loadBackups(instances);
 
-    for (let instance of BACKUP_INSTANCES) {
-      if (!hasBackupToday(instance)) {
-        createBackup(instance);
-      }
-
-      pruneBackups(instance);
+  const promises = instances.reduce((promises, instance) => {
+    if (!hasBackupToday(instance)) {
+      promises.push(createBackup(instance));
     }
-  })();
-};
 
-async function loadBackups() {
+    return promises.concat(pruneBackups(instance));
+  }, []);
+
+  await Promise.all(promises);
+}) as ScheduledHandler;
+
+async function loadBackups(instances: string[]) {
   let page = 1;
   console.log(`Loading all snapshots (page ${page})`);
 
-  let result = (await lightsail.getInstanceSnapshots().promise());
+  let result = await lightsail.getInstanceSnapshots().promise();
   let snapshots = result.instanceSnapshots;
 
   // Get all pages, if applicable.
   while (result.nextPageToken) {
     page++;
     console.log(`Loading all snapshots (page ${page})`);
-    result = (await lightsail.getInstanceSnapshots({pageToken: result.nextPageToken}).promise());
+    result = await lightsail
+      .getInstanceSnapshots({ pageToken: result.nextPageToken })
+      .promise();
     snapshots = snapshots.concat(result.instanceSnapshots);
   }
 
@@ -89,7 +85,7 @@ async function loadBackups() {
     let instance = snapshot.fromInstanceName;
 
     // Ignore other instances.
-    if (!shouldBackupInstance(instance)) continue;
+    if (!instances.includes(instance)) continue;
 
     // Create an array per instance.
     backups[instance] = backups[instance] || [];
@@ -97,8 +93,8 @@ async function loadBackups() {
   }
 
   // Sort by date.
-  for (let [instance, instanceBackups] of Object.entries(backups)) {
-    instanceBackups.sort((a, b) => a.createdAt - b.createdAt);
+  for (let [, instanceBackups] of Object.entries(backups)) {
+    instanceBackups.sort((a, b) => +a.createdAt - +b.createdAt);
   }
 }
 
@@ -108,11 +104,7 @@ async function loadBackups() {
  * @return {Boolean}
  */
 function isBackupSnapshot(snapshot) {
-  return snapshot.name.endsWith('-autosnap');
-}
-
-function shouldBackupInstance(instance) {
-  return BACKUP_INSTANCES.includes(instance);
+  return snapshot.name.endsWith("-autosnap");
 }
 
 function getLatestBackup(instance) {
@@ -142,13 +134,13 @@ function createBackup(instance) {
     instanceSnapshotName: name
   };
 
-  lightsail.createInstanceSnapshot(params, function(err, data) {
-    if (err) {
+  return lightsail
+    .createInstanceSnapshot(params)
+    .promise()
+    .then(() => console.log(`${instance}: Snapshot ${name} created`))
+    .catch(err => {
       console.error(`${instance}: Error creating snapshot`, err);
-    } else {
-      console.log(`${instance}: Snapshot ${name} created`);
-    }
-  });
+    });
 }
 
 function pruneBackups(instance) {
@@ -157,21 +149,27 @@ function pruneBackups(instance) {
   let instanceBackups = backups[instance];
   if (!instanceBackups) return;
 
+  const deletePromises: Promise<any>[] = [];
+
   for (let backup of instanceBackups) {
     let date = backup.createdAt;
     let dayOfWeek = date.getDay();
     let dayOfMonth = date.getDate();
-    let age = Math.floor((NOW - date) / ONE_DAY);
+    let age = Math.floor((+NOW - +date) / ONE_DAY);
 
     let saveBackup = false;
 
     if (age <= BACKUP_DAYS) {
-      console.log(`${instance}: Retaining daily backup from ${backup.createdAt}`);
+      console.log(
+        `${instance}: Retaining daily backup from ${backup.createdAt}`
+      );
       saveBackup = true;
     } else if (age <= BACKUP_WEEKS * 7) {
       // Is sunday?
       if (dayOfWeek === 0) {
-        console.log(`${instance}: Retaining weekly backup from ${backup.createdAt}`);
+        console.log(
+          `${instance}: Retaining weekly backup from ${backup.createdAt}`
+        );
         saveBackup = true;
       }
     } else if (age <= BACKUP_MONTHS * 30) {
@@ -179,16 +177,19 @@ function pruneBackups(instance) {
         dayOfMonth <= 7 && // Is first week of month?
         dayOfWeek === 0 // Is Sunday?
       ) {
-        console.log(`${instance}: Retaining monthly backup from ${backup.createdAt}`);
+        console.log(
+          `${instance}: Retaining monthly backup from ${backup.createdAt}`
+        );
         saveBackup = true;
       }
     }
 
     if (!saveBackup) {
       console.log(`${instance}: Deleting backup from ${backup.createdAt}`);
-      deleteSnapshot(backup);
+      deletePromises.push(deleteSnapshot(backup));
     }
   }
+  return deletePromises;
 }
 
 function deleteSnapshot(snapshot) {
@@ -196,11 +197,33 @@ function deleteSnapshot(snapshot) {
     instanceSnapshotName: snapshot.name
   };
 
-  lightsail.deleteInstanceSnapshot(params, function(err, data) {
-    if (err) {
-      console.error(`${snapshot.fromInstanceName}: Error deleting snapshot ${snapshot.name}`, err);
-    } else {
-      console.log(`${snapshot.fromInstanceName}: Snapshot ${snapshot.name} deleted`);
-    }
-  });
+  return lightsail
+    .deleteInstanceSnapshot(params)
+    .promise()
+    .then(() =>
+      console.log(
+        `${snapshot.fromInstanceName}: Snapshot ${snapshot.name} deleted`
+      )
+    )
+    .catch(err => {
+      console.error(
+        `${snapshot.fromInstanceName}: Error deleting snapshot ${
+          snapshot.name
+        }`,
+        err
+      );
+    });
+}
+
+async function getAllInstances() {
+  let allInstances: string[] = [];
+  let pageToken;
+  do {
+    const { instances, nextPageToken } = await lightsail
+      .getInstances({ pageToken })
+      .promise();
+    pageToken = nextPageToken;
+    allInstances = allInstances.concat(instances.map(i => i.name));
+  } while (pageToken);
+  return allInstances;
 }
--- test/test.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-var index = require('../src/index');
-index.handler(null, null, function(){});
